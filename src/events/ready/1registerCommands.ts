faa3d509225a94cd8b65503319532e1c855d7a8f
Tomsoz125 <razertomsoz@gmail.com>
2024-09-30 14:35:44 +0100

    changed commands

--- src/events/ready/1registerCommands.ts
+++ src/events/ready/1registerCommands.ts
@@ -2,34 +2,32 @@ import { Client, REST, Routes } from "discord.js";
 import fetchAllCmds from "../../utils/commands/fetchAllCmds";
 
 export = async (client: Client) => {
-	for (const g of await client.guilds.fetch()) {
-		(async () => {
-			const rest = new REST().setToken(process.env.BOT_TOKEN!);
+	(async () => {
+		const rest = new REST().setToken(process.env.BOT_TOKEN!);
 
-			try {
-				console.log(`Started refreshing application (/) commands`);
-				let cmds = [];
-				const commands = await fetchAllCmds();
+		try {
+			console.log(`Started refreshing application (/) commands`);
+			let cmds = [];
+			const commands = await fetchAllCmds();
 
-				for (const cmd of commands) {
-					const enabled = cmd.enabled;
-					if (enabled) cmds.push(cmd.data.toJSON());
-				}
-
-				const data: any = await rest.put(
-					Routes.applicationCommands(client.user!.id),
-					{
-						body: cmds
-					}
-				);
-
-				console.log(
-					`Successfully reloaded ${data.length} application (/) commands`
-				);
-			} catch (error) {
-				// And of course, make sure you catch and log any errors!
-				console.error(error);
+			for (const cmd of commands) {
+				const enabled = cmd.enabled;
+				if (enabled) cmds.push(cmd.data.toJSON());
 			}
-		})();
-	}
+
+			const data: any = await rest.put(
+				Routes.applicationCommands(client.user!.id),
+				{
+					body: cmds
+				}
+			);
+
+			console.log(
+				`Successfully reloaded ${data.length} application (/) commands`
+			);
+		} catch (error) {
+			// And of course, make sure you catch and log any errors!
+			console.error(error);
+		}
+	})();
 };
